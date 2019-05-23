3809edd6303748d5c0a7f9040e2f364341c3763e
dongwoo <>
2019-05-23 14:08:22 +0900

    modify config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  USER_BRANCH: '"local"'
+  NODE_ENV: '"development"'
 })
